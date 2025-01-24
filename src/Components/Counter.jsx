fc56fb5c2255b54b1ecd6586d7f51e5fb5293205
gideon <kipkerring.gedion@gmail.com>
2025-01-24 13:12:48 +0300

    counter application

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
+import "../Style.css";
 
 const Counter = () => {
+    const [count, setCount] = useState(0);
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
   return (
-    <div>Counter</div>
+    <div className='container'>
+        <div className='Container'>
+            <h1 className='number'>Count: {count}</h1>
+        </div>
+        <div className='btns-container'>
+            <button onClick={increment} className='increment'>+</button>
+            <button onClick={decrement} className='decrement'>-</button>
+        </div>
+    </div>
   )
 }
 
