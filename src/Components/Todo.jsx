3e193594a27e607c1c503519bfee4fa30da81fcd
gideon <kipkerring.gedion@gmail.com>
2025-01-25 15:02:15 +0300

    updated Todo application

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,30 +1,35 @@
 import React, { useState } from 'react'
 import "../Styles/todo.css"
 
+const generateId = () => {
+    return Math.floor(Math.random() * 10)
+}
+
 const Todo = () => {
     const [todos, setTodos] = useState([]);
     const [inputvalue, setInputValue] = useState("");
 
 
     const handleSubmit = () => {
-        setTodos((todos) => {
-            return todos.concat(() => {
-                text: input
-                id: Math.floor(Math.random() * 10)
-            });
-        });
-        setInputValue("");
-    };
-    const removeTodo = (id) => setTodos((todos) => todos.filter((t) => t.id !== id));
+        if (inputvalue.trim()) {
+            setTodos((prevTodos) => [...prevTodos, {
+                text: inputvalue,
+                id: generateId(),
+            }] )
+            setInputValue("");
+            
+        }
+    }
+    const removeTodo = (id) => setTodos((prevTodos) => prevTodos.filter((t) => t.id !== id));
   return (
     <div className='container'>
         <input type="text" placeholder='New Todo' value={inputvalue} onChange={(e) => setInputValue(e.target.value)} />
         <button onClick={handleSubmit}>Submit</button>
         <ul className='todos-list'>
-            {todos.map((text, id) => (
-                <li className='todo' key={id}>
-                    <span>{text}</span>
-                    <button className='close' onClick={removeTodo}>X</button>
+            {todos.map((todo) => (
+                <li className='todo' key={todo.id}>
+                    <span>{todo.text}</span>
+                    <button className='close' onClick={(id) => removeTodo(todo.id)}>X</button>
                 </li>
             ))}
         </ul>
