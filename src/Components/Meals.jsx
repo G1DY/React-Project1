221c9c3cf5781f74ab61ea1125ceb40638a7bfe3
gideon <kipkerring.gedion@gmail.com>
2025-01-25 15:21:48 +0300

    meal card application

--- src/Components/Meals.jsx
+++ src/Components/Meals.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import Axios from "axios";
+import axios from "axios";
 import { useState, useEffect } from 'react'
 import "../Styles/meals.css"
+import axios from 'axios';
 
 const Meals = () => {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then((res) => {
+            setItems(res.data.meals);
+        }).catch((err) => {
+            console.log(err)
+        });
+    }, []);
   return (
-    <div>Meals</div>
+    <div className='items-container'>{itemslist}</div>
   )
 }
 
