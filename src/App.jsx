3aa1f9cfb95438ccf83b5c25d62546b4392960d7
gideon <kipkerring.gedion@gmail.com>
2025-01-27 08:27:20 +0300

    add  data on the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import Testimonials from './Components/Testimonials'
+import { accordionData } from './Components/Utils/content'
+import Accordion from './Components/Accordion'
 
 const App = () => {
   return (
-    <Testimonials/>
+    <div className="accordion">
+      {accordionData.map((title, content) => (
+        <Accordion title={title} content={content}/>
+      ))}
+    </div>
   )
 }
 
--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "../Styles/accordion.css"
+import { useState } from 'react'
 
-const Accordion = () => {
+
+const Accordion = ({title, content}) => {
+    const [isactive, setIsActive] = useState(false);
   return (
-    <div>Accordion</div>
+    <section className="accordion-card" key={Math.random()}>
+        <div className="header" onClick={() => setIsActive(!isactive)}>
+            <div>
+                {title}
+                <p className="icon">{isactive ? "-" : "+"}</p>
+            </div>
+        </div>
+        <div className="content">{isactive && 
+            <p className="card-info">{content}</p>
+            }</div>
+    </section>
   )
 }
 
