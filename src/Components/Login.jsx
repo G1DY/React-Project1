0f190bdff1cfdd56c282efa8b14c0e93ca6bbfbf
gideon <kipkerring.gedion@gmail.com>
2025-01-28 08:11:17 +0300

    use states for the login form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,7 +3,20 @@ import { useState } from 'react'
 import "../Styles/Login.css"
 
 const Login = () => {
+    const [username, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [passwaord, setPassword] = useState('');
+    const [confirmpassword, setConfirmPassword] = ('');
 
+    const [usernamecolor, setUserNameColor] = useState('');
+    const [emailcolor, setEmailColor] = useState('');
+    const [passwordcolor, setPasswordColor] = useState('');
+    const [confirmpasswordcolor, setConfirmPasswordColor] = useState('');
+
+    const [errorusername, setErrorUserName] = useState('');
+    const [erroremail, setErrorEmail] = useState('');
+    const [errorpassword, setErrorPasssword] = useState('');
+    const [errorconfirmpassword, setErrorConfirmPassword] = useState('');
   return (
     <div>Login</div>
   )
